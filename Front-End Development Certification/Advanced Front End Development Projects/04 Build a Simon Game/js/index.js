8192a12d00ee1f5fe301808a74bb2e2e34bdd550
lpezda <lukasz.pezda@me.com>
2017-01-09 20:32:35 +0100

    Add on/off button's listener

--- Front-End Development Certification/Advanced Front End Development Projects/04 Build a Simon Game/js/index.js
+++ Front-End Development Certification/Advanced Front End Development Projects/04 Build a Simon Game/js/index.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
+  
+  //Setting  start options
+  var gameStartStatus = false;
+  
+  //Adding listener to on/off button
+  $("#onOffButton").click(clickOnOffButton);
 
-  
-  
+  //Functon use when on/off button is clicked
+  function clickOnOffButton() {
+    
+    if (gameStartStatus) {
+      $("#count").css("color", "#000000");
+      $("#onOffButton").text("On");
+      gameStartStatus = false;
+    } else {
+      $("#count").css("color", "#CC0000");
+      $("#onOffButton").text("Off");
+      gameStartStatus = true;
+    }
+  }
   
   
 });
